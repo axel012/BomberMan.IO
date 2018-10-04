72230c365e559878fde30aab803e20ca22bfdb31
axel012 <axel_012@hotmail.com.ar>
2018-10-04 16:40:37 -0300

    tank update

--- research/move/test.js
+++ research/move/test.js
@@ -14,12 +14,16 @@ function draw(){
     tank.handleKeys();
     tank.update();
     tank.display();
+    noStroke();
     fill(255);
-    for(var i=1;i<8;i+=2)
-    ellipse(tank.x  + tank.w * cos(tank.rotation + i*PI/4) -7  * cos(tank.rotation + i*PI/4) ,tank.y  + tank.w * sin(tank.rotation + i*PI/4) -7 * sin(tank.rotation + i*PI/4),14,14);
+    text(frameRate(),50,50);
+    //fill(255);
+    //for(var i=1;i<8;i+=2)
+   // ellipse(tank.x  + tank.w * cos(tank.rotation + i*PI/4) -7  * cos(tank.rotation + i*PI/4) ,tank.y  + tank.w * sin(tank.rotation + i*PI/4) -7 * sin(tank.rotation + i*PI/4),14,14);
   //  ellipse(tank.x  + tank.w * cos(tank.rotation + 3*PI/4) -7  * cos(tank.rotation + 3*PI/4) ,tank.y  + tank.w * sin(tank.rotation + 3*PI/4) -7  * sin(tank.rotation + 3*PI/4),14,14);
    // ellipse(tank.x  + tank.w * cos(tank.rotation + 5*PI/4) -7  * cos(tank.rotation + 5*PI/4) ,tank.y  + tank.w * sin(tank.rotation + 5*PI/4) -7  * sin(tank.rotation + 5*PI/4),14,14);
 }
+
 
 class Tank{
     constructor(img,x,y){
@@ -70,6 +74,22 @@ class Tank{
         this.rotation += this.rSpeed;
         this.x += this.xMove;
         this.y += this.yMove;
+        var dx = width - this.x;
+        var dx2 = 0 - this.x;
+        var dy = height - this.y;
+        var dy2 = 0 - this.y;
+        var r = this.w/2;
+   
+        if(dx*dx <= r*r){
+            this.x = width - r;
+        }else if(dx2*dx2 <= r*r){
+            this.x = r;
+        }
+        if(dy*dy <= r*r){
+            this.y = height - r;
+        }else if(dy2*dy2 <= r*r){
+            this.y = r;
+        }
     }
 
     display(){
@@ -78,14 +98,23 @@ class Tank{
         noFill();
         stroke(0,255,0);
         strokeWeight(2);
-        rect(-this.w/2,-this.w/2,this.w,this.w);
+//        rect(-this.w/2,-this.w/2,this.w,this.w);
         rotate(this.rotation);
         image(this.img,-this.w/2,-this.w/2);
        stroke(0,0,255);
       //  fill(255);
-        rect(-this.w/2,-this.w/2,this.w,this.w);
+  //      rect(-this.w/2,-this.w/2,this.w,this.w);
+        
         //fill(255,0,0);
         //ellipse(this.w/2,0,15,15);
+     
         pop();
+        strokeWeight(2);
+        stroke(255);
+      //  line(this.x,this.y,this.x , this.y + 50 * sin(this.rotation));
+    //    line(this.x,this.y,this.x + 50 * cos(this.rotation) , this.y);
+       // line(this.x,this.y,this.x + 50 * cos(this.rotation),this.y + 50 * sin(this.rotation));
+        noFill();
+        ellipse(this.x,this.y,this.w,this.w);
     }
 }

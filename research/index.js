5b83057658bbf4823e17c3a8398a5e4bbbb27e87
axel012 <axel_012@hotmail.com.ar>
2018-10-12 21:10:47 -0300

    aaaaaaaaaaaa

--- research/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const server = require('http').createServer(app);
-
-app.use(express.static(path.join(__dirname, 'main')));
-server.listen(3000,"0.0.0.0", function() {
-});
--- research/main/maps/blockTiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles" tilewidth="32" tileheight="32" tilecount="25" columns="5">
- <image source="blockTiles.png" width="160" height="160"/>
-</tileset>
